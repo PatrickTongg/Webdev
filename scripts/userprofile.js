d8efe81a0b0c4a9c7e53e567a5ea4fd94c4635bb
Patrick Tong Mac <tongchakpupatrick@gmail.com>
2023-12-02 12:32:55 -0500

    created auto on list function

--- scripts/userprofile.js
+++ scripts/userprofile.js
@@ -1,25 +1,37 @@
 
 var items = JSON.parse(window.localStorage.getItem('items'))
+var users = JSON.parse(window.localStorage.getItem('users'))
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.getElementById(selector);
 
+console.log(users)
 
 //create html for the items
-function createItemList(){
-    var soldItem = $$('#soldItems')
-    var result;
+function createSoldItemList(){
+    var soldItem = $$('soldItems')
+    var result = '';
     for (let item of items){
-        result += " <div class="product"><img class="productImage" src="/public/images/laptop.png" alt="laprop">
-        <a href="item-page.html" class="productLink"></a>
-        <div class="product_price">$750</div></div>"
-    
+        result += '<div class="product"><img class="productImage" src="'+item.images[0]+'" alt='+item.name+'><a href="" class="productLink">'+item.name+'</a><div class="product_price">$'+item.price+'</div></div>';
     }
     soldItem.innerHTML += result; 
+}
+
+function createBuyItemList(){
+    var buyItems = $$('buyItems')
+    var result = '';
+    for (let item of items){
+        result += '<div class="product"><img class="productImage" src="'+item.images[0]+'" alt='+item.name+'><a href="" class="productLink">'+item.name+'</a><div class="product_price">$'+item.price+'</div></div>';
+        console.log(result);
+    }
+    buyItems.innerHTML += result; 
 }
 //create html for the personal info
 function createInfo(){
 
+
 }
 window.onload = () => {
-    createItemList()
+    createSoldItemList()
+    createBuyItemList()
+
 }
